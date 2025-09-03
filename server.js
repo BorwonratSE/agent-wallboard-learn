a08b099b1196eab80eb2955fe19855ab27999275
BorwonratSE <borwonrat_si67@live.rmutl.ac.th>
2025-09-03 17:36:26 +0700

    Checkpoint 2.1

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ const app = express(); // เติมให้ถูก
 // ขั้นที่ 3: กำหนด PORT
 const PORT = 3001;
 
+// Agent sample data
+let agents = [
+  {
+    code: "A001",
+    name: "John Doe",
+    status: "Available",
+  },
+  {
+    code: "A002",
+    name: "Alice Smith",
+    status: "Busy",
+  },
+  {
+    code: "A003",
+    name: "Bob Lee",
+    status: "Offline",
+  }
+];
+
+
 // ขั้นที่ 4: สร้าง route แรก
 app.get('/', (req, res) => {
     res.send("Hello Agent Wallboard!");
